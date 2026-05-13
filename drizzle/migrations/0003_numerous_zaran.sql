49fed31ba55c5b9a45c503a21f8a18866ac776bc
ItsLhuis <itslhuis.dev@gmail.com>
2026-05-13 14:59:28 +0100

    chore(database): remove deprecated settings columns

---
+++ drizzle/migrations/0003_numerous_zaran.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "settings" DROP COLUMN "base_url";--> statement-breakpoint
+ALTER TABLE "settings" DROP COLUMN "sentry_dsn";--> statement-breakpoint
+ALTER TABLE "settings" DROP COLUMN "metrics_token";--> statement-breakpoint
+ALTER TABLE "settings" DROP COLUMN "hosted_mode";
