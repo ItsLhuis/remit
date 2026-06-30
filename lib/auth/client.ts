55e297d731e6a8043acc5fc410d89b0bbc911a7c
ItsLhuis <itslhuis.dev@gmail.com>
2026-06-30 10:33:14 +0100

    refactor(auth): remove unused signIn and signUp exports from auth client

--- lib/auth/client.ts
+++ lib/auth/client.ts
@@ -6,4 +6,4 @@ export const authClient = createAuthClient({
   plugins: [twoFactorClient(), organizationClient()]
 })
 
-export const { signIn, signOut, signUp, useSession } = authClient
+export const { signOut, useSession } = authClient
